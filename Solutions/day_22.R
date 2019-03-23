8a41f7e79edbb2f0dffbcbcb87f0e64fe16828f6
Paul <plschoenmakers@hotmail.com>
2019-03-23 11:49:10 +0100

    Update day_22.R

--- Solutions/day_22.R
+++ Solutions/day_22.R
@@ -1,1 +1,3 @@
 # I did it manually
+# Keep in mind that the poison attack is more cost effective than the other attacks and check how many attacks you need to kill the enemy
+# After that you can try to fit it with either shield or drain
